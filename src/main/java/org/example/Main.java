66d7d73853edde76de1f7b9870be84e92238f6d6
anatolii <anatoliise@mail.ru>
2024-03-23 23:02:57 +0300

    comit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,89 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        MayList mayList = new MayList();
+        System.out.println(Arrays.toString(mayList.arr));
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            Random random = new Random();
+            array[i] = random.nextInt(1, 99);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
         }
+        int[] array2 = Arrays.copyOf(array, 100);
+
+
+        System.out.println(Arrays.toString(array));
+        long start = System.currentTimeMillis();
+        sortBubble(array);
+
+        System.out.println(System.currentTimeMillis() - start);
+        System.out.println(Arrays.toString(array));
+        long start2 = System.currentTimeMillis();
+        sortSelection(array2);
+        System.out.println(System.currentTimeMillis() - start2);
+        System.out.println(Arrays.toString(array2));
+        System.out.println(contains(array, 41));
+
+
     }
+
+    public static int[] sortBubble(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static int[] sortSelection(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minElementIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minElementIndex]) {
+                    //minElementIndex = j;
+
+
+                    int temp = arr[j];
+                    arr[j] = arr[minElementIndex];
+                    arr[minElementIndex] = temp;
+                }
+
+            }
+        }
+            return arr;
+
+    }
+    public static int contains(int[] arr, int element) {
+        int min = 0;
+        int max = arr.length - 1;
+        int index=0;
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (element == arr[mid]) {
+                index= mid;
+            }
+
+            if (element < arr[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return index;
+    }
+
 }
